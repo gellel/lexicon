260b815428d991da667892c5aa05472069dc2550
gellel <lindsay.gelle@gmail.com>
2020-01-20 23:05:46 +1100

    Adding interface for Lex

--- lexicon.go
+++ lexicon.go
@@ -1,5 +1,23 @@
 package lexicon
 
+var _ lexer = (&Lex{})
+
+type lexer interface {
+	Add(interface{}, interface{}) *Lex
+	Del(interface{}) *Lex
+	Each(func(interface{}, interface{})) *Lex
+	EachBreak(func(interface{}, interface{}) bool) *Lex
+	EachKey(func(interface{})) *Lex
+	EachValue(func(interface{})) *Lex
+	Fetch(interface{}) interface{}
+	Get(interface{}) (interface{}, bool)
+	Has(interface{}) bool
+	Keys() []interface{}
+	Len() int
+	Map(func(interface{}, interface{}) interface{}) *Lex
+	Values() []interface{}
+}
+
 // Lex is an implementation of a *map[interface{}]interface{}.
 //
 // Lex has methods to perform traversal and mutation operations.
@@ -20,7 +38,7 @@ func (lex *Lex) Del(k interface{}) *Lex {
 }
 
 // Each executes a provided function once for each map element.
-func (lex *Lex) Each(fn func(interface{}, interface{})) *Lex {
+func (lex *Lex) Each(fn func(k interface{}, v interface{})) *Lex {
 	var (
 		k, v interface{}
 	)
@@ -46,6 +64,29 @@ func (lex *Lex) EachBreak(fn func(interface{}, interface{}) bool) *Lex {
 	return lex
 }
 
+// EachKey executes a provided function once for each key in the map.
+func (lex *Lex) EachKey(fn func(k interface{})) *Lex {
+	lex.Each(func(k, _ interface{}) {
+		fn(k)
+	})
+	return lex
+}
+
+// EachValue executes a provided function once for each value in the map.
+func (lex *Lex) EachValue(fn func(v interface{})) *Lex {
+	lex.Each(func(_, v interface{}) {
+		fn(v)
+	})
+	return lex
+}
+
+// Fetch retrieves the element held by the argument key.
+// Returns nil if key does not exist.
+func (lex *Lex) Fetch(k interface{}) interface{} {
+	var v, _ = lex.Get(k)
+	return v
+}
+
 // Get gets the element from the map at the key address.
 // Returns a bool if the element was found using the key.
 func (lex *Lex) Get(k interface{}) (interface{}, bool) {
@@ -57,4 +98,38 @@ func (lex *Lex) Get(k interface{}) (interface{}, bool) {
 func (lex *Lex) Has(k interface{}) bool {
 	var _, ok = lex.Get(k)
 	return ok
+}
+
+// Keys returns a slice of the maps keys in the order found.
+func (lex *Lex) Keys() []interface{} {
+	var (
+		s = []interface{}{}
+	)
+	lex.Each(func(k, _ interface{}) {
+		s = append(s, k)
+	})
+	return s
+}
+
+// Len returns the number of elements in the map.
+func (lex *Lex) Len() int { return (len(*lex)) }
+
+// Map executes a provided function once for each element in the map and sets
+// the returned value to the current key.
+func (lex *Lex) Map(fn func(k interface{}, v interface{}) interface{}) *Lex {
+	lex.Each(func(k interface{}, v interface{}) {
+		lex.Add(k, fn(k, v))
+	})
+	return lex
+}
+
+// Values returns a slice of the map values in order found.
+func (lex *Lex) Values() []interface{} {
+	var (
+		s = []interface{}{}
+	)
+	lex.Each(func(_, v interface{}) {
+		s = append(s, v)
+	})
+	return s
 }
