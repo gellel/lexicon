6344c7494d578a9f1cd78ed4d5be284d849a34d7
gellel <lindsay.gelle@gmail.com>
2020-02-18 21:15:21 +1100

    Adding tests

--- lex_test.go
+++ lex_test.go
@@ -1,15 +1,33 @@
 package lex_test
 
 import (
-	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/gellel/lex"
 )
 
+var (
+	l = (*&lex.Lex{})
+)
+
 func Test(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func TestAdd(t *testing.T) {
 	var (
-		l = (&lex.Lex{})
+		k = rand.Intn(10)
+		v = rand.Intn(10)
 	)
-	fmt.Println(l)
+	l.Add(k, v)
+	x, ok := (l)[k]
+	if !ok {
+		t.Fatalf("(&lex.Lex.Add(interface{}, interface{}) (_, bool)) != true")
+	}
+	ok = (x == v)
+	if !ok {
+		t.Fatalf("(&lex.Lex.Add(interface{}, interface{}) (interface{}, _)) != interface{}")
+	}
 }
