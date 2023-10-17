d2d1b99458b21ccc4ad22da9f16ad8586d6b1a91
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-10-17 12:13:39 +1100

    Dev (#16)
    * Dev

--- hashtable.go
+++ hashtable.go
@@ -33,3 +33,76 @@ func (hashtable *Hashtable[K, V]) Add(key K, value V) *Hashtable[K, V] {
 	(*hashtable)[key] = value
 	return hashtable
 }
+
+// AddFunc inserts key-value pairs into the hashtable based on the provided maps and a custom validation function.
+// The validation function should return true for key-value pairs that should be added to the hashtable, and false otherwise.
+//
+// Example:
+//   ht := make(hashtable.Hashtable[string, int])
+//   ht.AddFunc([]map[string]int{{"apple": 1, "orange": 2}}, func(key string, value int) bool {
+//       // Only add key-value pairs where the value is greater than 1.
+//       return value > 1
+//   })
+func (hashtable *Hashtable[K, V]) AddFunc(values []map[K]V, fn func(key K, value V) bool) *Hashtable[K, V] {
+	for _, item := range values {
+		for key, value := range item {
+			if fn(key, value) {
+				hashtable.Add(key, value)
+			}
+		}
+	}
+	return hashtable
+}
+
+// AddMany inserts multiple key-value pairs into the hashtable from the provided maps.
+// If keys already exist, their associated values are updated.
+//
+// Example:
+//   ht := make(hashtable.Hashtable[string, int])
+//   ht.AddMany(map[string]int{"orange": 7, "grape": 4}, map[string]int{"kiwi": 6, "pear": 9})
+func (hashtable *Hashtable[K, V]) AddMany(values ...map[K]V) *Hashtable[K, V] {
+	for _, item := range values {
+		for key, value := range item {
+			hashtable.Add(key, value)
+		}
+	}
+	return hashtable
+}
+
+// Delete removes a key-value pair from the hashtable based on the provided key.
+//
+// Example:
+//   ht := make(hashtable.Hashtable[string, int])
+//   ht.Add("apple", 5)
+//   ht.Add("banana", 3)
+//   ht.Delete("apple") // Removes the key-value pair with key "apple" from the hashtable.
+func (hashtable *Hashtable[K, V]) Delete(key K) *Hashtable[K, V] {
+	delete(*hashtable, key)
+	return hashtable
+}
+
+// Get retrieves the value associated with the provided key from the hashtable.
+// If the key exists, it returns the associated value and true. Otherwise, it returns the zero value for the value type and false.
+//
+// Example:
+//   ht := make(hashtable.Hashtable[string, int])
+//   ht.Add("apple", 5)
+//   value, exists := ht.Get("apple") // 5, true
+//   value, exists = ht.Get("orange")  // 0, false
+func (hashtable *Hashtable[K, V]) Get(key K) (V, bool) {
+	value, ok := (*hashtable)[key]
+	return value, ok
+}
+
+// Has checks if the provided key exists in the hashtable.
+// It returns true if the key exists, and false otherwise.
+//
+// Example:
+//   ht := make(hashtable.Hashtable[string, int])
+//   ht.Add("apple", 5)
+//   exists := ht.Has("apple") // true
+//   exists = ht.Has("orange")  // false
+func (hashtable *Hashtable[K, V]) Has(key K) bool {
+	_, ok := (*hashtable)[key]
+	return ok
+}
